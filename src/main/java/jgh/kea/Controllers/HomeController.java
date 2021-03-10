890a8780bea5cfc29be5d364d622712019ed15d7
JGHarder <Jacob.gade.harder@gmail.com>
2021-03-10 10:57:07 +0100

    added more css Form link now works removed unused classes

--- src/main/java/jgh/kea/Controllers/HomeController.java
+++ src/main/java/jgh/kea/Controllers/HomeController.java
@@ -1,6 +1,5 @@
 package jgh.kea.Controllers;
 
-import jgh.kea.Services.Ingredient;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -9,17 +8,10 @@ import org.springframework.web.bind.annotation.*;
 public class HomeController {
 
     @GetMapping("/")
-        public String index(Model model){
-        model.addAttribute("ingredient", new Ingredient());
+        public String index(){
         return "index.html";
     }
 
-    @PostMapping("/")
-    public String submit(@ModelAttribute Ingredient ingredient, Model model ){
-model.addAttribute("ingredient", ingredient);
-
-        return "result";
-    }
 
     @GetMapping("/fakta")
     public String fakta(){
@@ -29,10 +21,5 @@ model.addAttribute("ingredient", ingredient);
 
 
 
-    @GetMapping(value="/submitted")
-    public String submitted(@RequestParam String ingredient, Model model) {
-model.getAttribute(ingredient);
-        return "result";
-    }
 }
 
