bd0fd73ac4289a2d411bfd56ed1a46c7fb3d44ae
Rabia <ahmed-rabia@hotmail.com>
2021-03-10 10:57:20 +0100

    updated project

--- src/main/java/jgh/kea/Controllers/HomeController.java
+++ src/main/java/jgh/kea/Controllers/HomeController.java
@@ -1,6 +1,6 @@
 package jgh.kea.Controllers;
 
-import jgh.kea.Services.Ingredient;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -9,16 +9,8 @@ import org.springframework.web.bind.annotation.*;
 public class HomeController {
 
     @GetMapping("/")
-        public String index(Model model){
-        model.addAttribute("ingredient", new Ingredient());
+        public String index(){
         return "index.html";
-    }
-
-    @PostMapping("/")
-    public String submit(@ModelAttribute Ingredient ingredient, Model model ){
-model.addAttribute("ingredient", ingredient);
-
-        return "result";
     }
 
     @GetMapping("/fakta")
@@ -27,12 +19,5 @@ model.addAttribute("ingredient", ingredient);
         return "fakta";
     }
 
-
-
-    @GetMapping(value="/submitted")
-    public String submitted(@RequestParam String ingredient, Model model) {
-model.getAttribute(ingredient);
-        return "result";
-    }
 }
 
